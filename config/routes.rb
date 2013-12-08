54cebbd85af1c8e60eec0f0d943f6723dcfde4ec
ppaulauskas <paulauskas@alumni.princeton.edu>
2013-12-08 00:57:58 -0600

    Do CSS Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Concerts::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
 
   resources :users
