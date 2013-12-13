f585c99820a845ee057dc9f9a122c8eab45fefe7
ppaulauskas <paulauskas@alumni.princeton.edu>
2013-12-13 02:49:36 -0600

    Finish user sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Fisheye!"
+      flash[:success] = "Welcome to Fisheye!!"
       redirect_to @user
     else
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_secure_password
   before_save { self.email = email.downcase }
   before_create :create_remember_token
+  has_secure_password
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 
   private
 
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
