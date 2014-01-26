8eb20abe878d4fbc58e2bffee400f24388f47e73
ppaulauskas <paulauskas@alumni.princeton.edu>
2014-01-26 05:58:47 -0600

    Clean up migrations

--- db/schema.rb
+++ db/schema.rb
@@ -16,46 +16,14 @@ ActiveRecord::Schema.define(version: 20140126055202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "band_info", id: false, force: true do |t|
-    t.string  "name",      limit: 64,  null: false
-    t.text    "related",                            array: true
-    t.integer "rating"
-    t.string  "image",     limit: 64
-    t.string  "lastfmurl", limit: 128
-  end
-
-  create_table "band_info_temp", id: false, force: true do |t|
-    t.string  "name",      limit: 64
-    t.text    "related",               array: true
-    t.integer "rating"
-    t.string  "image",     limit: 64
-    t.string  "lastfmurl", limit: 128
-  end
-
-  create_table "bands", id: false, force: true do |t|
-    t.string  "name",      limit: 64,  null: false
-    t.text    "related",                            array: true
-    t.integer "rating"
-    t.string  "image",     limit: 64
-    t.string  "lastfmurl", limit: 128
-  end
-
-  create_table "event_info", id: false, force: true do |t|
-    t.integer "event_id",              null: false
-    t.string  "title",     limit: 128
-    t.string  "headliner", limit: 64
-    t.text    "artists",                            array: true
-    t.integer "venue_id"
-    t.text    "eventtime"
-  end
-
-  create_table "event_info_temp", id: false, force: true do |t|
-    t.integer  "event_id",              null: false
-    t.string   "title",     limit: 128
-    t.string   "headliner", limit: 64
-    t.text     "artists",                            array: true
-    t.integer  "venue_id"
-    t.datetime "eventtime"
+  create_table "bands", force: true do |t|
+    t.string   "name"
+    t.text     "related",    default: [], array: true
+    t.integer  "rating"
+    t.string   "image"
+    t.string   "lastfmurl"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "events", primary_key: "event_id", force: true do |t|
@@ -66,10 +34,6 @@ ActiveRecord::Schema.define(version: 20140126055202) do
     t.datetime "eventtime"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "lastfm_nyc_raw", primary_key: "row_id", force: true do |t|
-    t.text "data"
   end
 
   create_table "users", force: true do |t|
@@ -84,32 +48,6 @@ ActiveRecord::Schema.define(version: 20140126055202) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
-
-  create_table "venue_info", id: false, force: true do |t|
-    t.integer "venue_id",              null: false
-    t.string  "name",      limit: 64
-    t.float   "lat"
-    t.float   "long"
-    t.string  "city",      limit: 64
-    t.string  "country",   limit: 64
-    t.string  "street",    limit: 128
-    t.string  "postal",    limit: 16
-    t.string  "lastfmurl", limit: 128
-    t.string  "website",   limit: 128
-  end
-
-  create_table "venue_info_temp", id: false, force: true do |t|
-    t.integer "venue_id"
-    t.string  "name",      limit: 64
-    t.float   "lat"
-    t.float   "lon"
-    t.string  "city",      limit: 64
-    t.string  "country",   limit: 64
-    t.string  "street",    limit: 128
-    t.string  "postal",    limit: 16
-    t.string  "lastfmurl", limit: 128
-    t.string  "website",   limit: 128
-  end
 
   create_table "venues", primary_key: "venue_id", force: true do |t|
     t.string   "name"
