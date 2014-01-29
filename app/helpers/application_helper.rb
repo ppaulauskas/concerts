71ae177c2c48c0b157f9adcc54ccf1a20cfe54ab
ppaulauskas <paulauskas@alumni.princeton.edu>
2014-01-29 03:42:10 -0600

    Update header, styling changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   #Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Fisheye"
+    base_title = "Sickday At"
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} #{page_title}"
     end
   end
 end
