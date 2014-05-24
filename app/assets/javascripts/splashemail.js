75102f4cd086b51f0520bd40d4d21d969dfb79cb
ppaulauskas <paulauskas@alumni.princeton.edu>
2014-05-23 19:37:26 -0500

    Add popup and tab navigation

--- app/assets/javascripts/splashemail.js
+++ app/assets/javascripts/splashemail.js
@@ -106,6 +106,25 @@ onload=function(){
 	});
 };
 
+
+$(document).ready(function() {
+  $('.popup').click(function(event) {
+    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+    
+    window.open(url, $(this).attr('class')[0], opts);
+ 
+    return false;
+  });
+});
 /*
 onload=function(){
     var text=document.getElementsByName('entry.428211031')[0];
